3ded6121a0fa965155c8b200f5b760d6a03900f7
Anson Li <ansonli@Ansons-MacBook-Air-3.local>
2025-07-19 14:28:28 -0400

    Started new template for the main app

---
+++ src/Views/TopFold.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TopFold.css';
+import milkyWay from '../assets/images/Milky-Way-Cookie-V1.png';
+import milkyWayLogo from '../assets/images/logo512.png';
+
+const TopFold = () => {
+    return (
+      <div className="header">
+            
+      </div>
+    );
+};
+
+export default TopFold;
